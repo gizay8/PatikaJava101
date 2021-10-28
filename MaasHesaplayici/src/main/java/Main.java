6665ca297be6c98a205911b18bd1dd27e50170bd
gizay8 <91142274+gizay8@users.noreply.github.com>
2021-10-28 15:25:36 +0300

    değişiklik

--- MaasHesaplayici/src/main/java/Main.java
+++ MaasHesaplayici/src/main/java/Main.java
@@ -1,4 +1,4 @@
-//Maaş hesabı yapan sınıf
+//Maaş hesabı yapan program
 public class Main {
     public static void main(String[] args) {
         Employee e1 = new Employee("Kemal", 2000.0, 45, 1985);
